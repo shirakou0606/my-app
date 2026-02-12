85cdb41174ea80be6d4693b49ac9451219e3e476
shirakou0606 <shirakou0606@gmail.com>
2026-02-13 02:53:09 +0900

    Update logout.js

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,9 +1,27 @@
-exports.handler = async () => {
+const ORIGIN = "https://silly-empanada-4cfc7a.netlify.app";
+
+function corsHeaders() {
+  return {
+    "Access-Control-Allow-Origin": ORIGIN,
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "POST,OPTIONS,GET",
+    "Vary": "Origin"
+  };
+}
+
+exports.handler = async (event) => {
+  if (event.httpMethod === "OPTIONS") {
+    return { statusCode: 204, headers: corsHeaders(), body: "" };
+  }
+
   return {
     statusCode: 200,
     headers: {
+      ...corsHeaders(),
       "Set-Cookie": "session=; HttpOnly; Secure; SameSite=Lax; Path=/; Max-Age=0",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "Cache-Control": "no-store"
     },
     body: JSON.stringify({ ok: true })
   };
